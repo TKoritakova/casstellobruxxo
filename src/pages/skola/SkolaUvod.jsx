787bfe47b460e663558d254b8d894e0977fcb718
Tereza Koriťáková <koritter@fel.cvut.cz>
2026-02-23 11:55:42 +0100

    uprava skoly

--- src/pages/skola/SkolaUvod.jsx
+++ src/pages/skola/SkolaUvod.jsx
@@ -14,76 +14,14 @@ const SkolaUvod = () => (
     
 
   
-
-  <h2>Školní uniformy</h2>
-  <hr />
   
-  <img src="https://i.postimg.cc/TYnWbRMY/ezgif-680fa215553947-1.png" className='img-in-text img-in-text-right' />
-  
-  <p>Velmi důležitou a výraznou součástí uniformy jsou <span className='text-highlighter'>nepromokavé zelené pláště</span> z lehkého materiálu, které ukazují na spojení školy s přírodou a džunglí, v níž se nachází. Zdobené jsou zlatým lemováním a tradičními symboly. U dolního lemu je možné vidět kené, tradiční umění složené z geometrických tvarů, které vyjadřují spiritualitu. Nad kené je odkaz na amazonské artefakty, kterým se říká muiraquitãs. Tyto artefakty měly velmi často podobu žáby vyřezané do dřeva nebo kamene a sloužily na ochranu.</p> 
-
-  <p>Pod pláštěm studenti nosí <span className='text-highlighter'>každodenní oblečení</span> složené ze zeleného vršku, který symbolizuje listí, a hnědých kalhot, které vzhledem připomínají kůru. Kalhoty jsou součástí i dívčí verze uniformy z důvodu praktičnosti pro snazší pohyb po lese. Oblečení je volné, pohodlné a snadné k oblékání.</p>
-  
-  <img src="https://i.postimg.cc/mDQGw6bN/ezgif-61d6089def7b7e-1.png" className='img-in-text img-in-text-left' />
-
-  <p>Důležitou součástí každodenního oděvu je <span className='text-highlighter'>pásek, na který je možné přivazovat menší taštičky a váčky</span>, ve kterých je možné nosit vše, co bude student během dne potřebovat – od přísad do lektvarů a potřeb pro rituály, až po krmení pro nejrůznější tvory.</p> 
-
-  <p>Pro <span className='text-highlighter'>slavnosti</span> a další zvláštní příležitosti mají studenti druhou verzi oblečení pod plášť. Ta se skládá z bělavého vršku, který je navíc dozdobený dalšími geometrickými vzory. Dívčí verze uniformy obsahuje v tomto případě sukni, protože zde již není praktičnost prioritou. Spodní části obou verzí jsou v zelené barvě se zlatým dolním lemem. Ani tentokrát uniformám nechybí pásek, který je v tomto případě určen na uchycení tradičních barevných ozdob, kterými jsou šátky, peříčka, třásničky a korále.</p> 
-
-  <p><span className='text-highlighter'>Barvy ozdob jsou určeny kmenovou příslušností.</span> Arashar (sladcí) mají růžové a zelené ozdoby, Pawanunga (slaní) hnědé a červené, Kuaraori (hořcí) žluté a oranžové a Nomphyrian (kyselí) fialové a tyrkysové.</p>
-
-  <img src="https://i.postimg.cc/FKTSKDtq/Untitled-162-1.png" className='img-in-text' />
-  <img src="https://i.postimg.cc/rpVZdVss/Untitled-163-1.png" className='img-in-text' />
-  <img src="https://i.postimg.cc/4yVQLqyQ/Untitled-165-1.png" className='img-in-text' />
-  <img src="https://i.postimg.cc/wB427Dnh/Untitled-164-1.png" className='img-in-text' />
-
-  <p>Ke každodenní uniformě patří i středně velká zelená <span className='text-highlighter'>taška</span>, kterou je možné pohodlně připevnit k pásku. I ta obsahuje kmenové barvy v ozdobných stužkách.</p>
-
-
-  <img src="https://i.postimg.cc/76Yvy10g/ezgif-60c3df23ef9a00-1.png" className='img-in-text'  />
 
   
 
-  <h2>Domácí mazlíčci</h2>
-  <hr />
   
-  <p>Počet mazlíčků je ve škole přísně omezen – každý student může mít <span className='text-highlighter'>maximálně dva</span>. Tento limit umožňuje, aby mazlíček měl zvířecího společníka, případně aby student vlastnil například sovu a jiného tvora současně. Jakékoliv výjimky, ať už co do počtu, nebo co do druhu zvířete, u něhož by bylo sporné, zda lze považovat za školního mazlíčka, je nutné předem projednat s vedením školy.</p>
-
-  <p>Povolují se tradičně chovaná zvířata lidmi žijícími v domech, jelikož se nehodí, aby se chodbami školy proháněla divoká zvířata či stáda skotu. Přípustná jsou zvířata v teráriích a akváriích jako <span className='text-highlighter'>rybičky</span>, <span className='text-highlighter'>žáby</span>, <span className='text-highlighter'>drobní ještěři</span>, <span className='text-highlighter'>želvy</span> a <span className='text-highlighter'>nejedovatí pavouci či hadi</span> do dvou metrů. Terária a akvária by ideálně neměla příliš přesahovat velikost psacích stolů a jejich rozměry je možno debatovat s kmenovým vedením. Co se drobných savců a ptactva týče, přípustné jsou domestikované <span className='text-highlighter'>kočky</span>, <span className='text-highlighter'>papoušci</span>, <span className='text-highlighter'>hlodavci</span> (včetně kluběnek), <span className='text-highlighter'>ježci</span> a <span className='text-highlighter'>malé opičky</span>.</p>
-
-
-  <img src="https://i.postimg.cc/JhyZx9S1/amazon-milk-frog-6679632-1280.jpg" className='img-in-text' />
-  <img src="https://i.postimg.cc/mrcCj0Kz/N-vrh-bez-n-zvu-4.png" className='img-in-text' />
-
-  <p>U všech mazlíčků je požadována jejich poslušnost a záruka majitele, že mazlíček nebude nijak narušovat chod školy a výuky na ní. Při opakovaných porušeních dochází k volání rodičům a posílání mazlíčků domů, při extrémních situacích ke kárným trestům. Škola je v tomto poměrně nekompromisní, jelikož si přeje <span className='text-highlighter'>eliminovat kontakt studentů s nebezpečnými tvory</span> bez dospělého dozoru a potřebné průpravy, kterou na tyto interakce výuka poskytuje.</p>
-
-  <h2>Školní stravování</h2>
-  <hr />
-  
-  <p>Školní stravování v Castelobruxu je velmi <span className='text-highlighter'>pestré a barevné</span>. Den začíná <span className='text-highlighter'>snídaní</span>, která se podává mezi sedmou a osmou hodinou ranní. Studenti mají možnost ve <span className='text-highlighter'>společenských prostorách</span> svých kmenů nalézt široký výběr čerstvého tropického ovoce, tradičního pečiva a sýrů. Nechybí ani pão de queijo, tapiokové palačinky, yerba maté či silná brazilská káva pro starší studenty. Snídaně probíhá v uvolněné atmosféře a je složena převážně z <span className='text-highlighter'>tradičnějších pokrmů</span>, nicméně některé dny lze nalézt i více severoamerická či evropská snídaňová jídla.</p>
-
-  <p><span className='text-highlighter'>Oběd je hlavním jídlem dne</span> a podává se od dvanácti do půl druhé. Studenti se opět stravují po kmenech, ačkoliv mohou být dny, kdy jsou obědy organizovány společně – zejména pokud probíhá slavnostní hostina. Na stolech bývá rýže, fazole, maniok a nejrůznější druhy masa či ryb. Tradičním pokrmem je <span className='text-highlighter'>feijoada</span>, která se v jídelně objevuje pravidelně, ale nikdy nechybí ani <span className='text-highlighter'>vegetariánské varianty</span> založené na luštěninách a zelenině. Ani obědy se vlivům z jiných kontinentů občas nevyhnou, takže je možné narazit na hamburger nebo pizzu.</p>
-
-  <img src="https://i.postimg.cc/rm53ttp0/Traditional-Mexican-Dishes-1.jpg" className='img-in-text' />
-  <img src="https://i.postimg.cc/KztwggYn/famous-brazilian-foods-feijoada-pao-de-queijo-1024x574.png" className='img-in-text' />
-
-  <p><span className='text-highlighter'>Večeře se považuje za společenský vrchol dne</span> a nese se v mnohem slavnostnějším duchu než snídaně a oběd. Celá škola usedá k dlouhým stolům a podávají se jídla inspirovaná různými částmi Brazílie a Latinské Ameriky – od amazonských rybích specialit přes baíaňské pokrmy s kokosovým mlékem, tacos, paellu, empanadas nebo tamales, až po churrasco, velké kusy masa otáčené na kouzelných rožních. Vedle tradičně masitých jídel jsou běžně nabízeny i ryby, vegetariánské a veganské varianty a díky hojnosti manioku a tapioky také bezlepková jídla. <span className='text-highlighter'>Do večeří se tolik nepromítají vlivy z jiných kontinentů.</span></p>
-
-  <p>Součástí večeře bývají i sladkosti – především brigadeiro, churros, ovoce, ale i další. Atmosféru doplňují poletující světelné lucerny, hudba a častý smích. Výjimkou nejsou ani spontánní taneční vložky, zejména o víkendech.</p>
-
-  <p>Mezi jídly si studenti mohou vyzvednout <span className='text-highlighter'>svačinky v bufetu</span> poblíž kuchyně. Tam je navíc připraveno i mnoho druhů nápojů, takže ani nejnáročnější studenti nemají výmluvu proč nedodržovat pitný režim. <span className='text-highlighter'>Stravování v Castelobruxu odráží pestrost brazilské a latino kultury a propojení školy s pralesem a jeho dary.</span> Jídlo je kvalitní, čerstvé a chutné.</p>
 
   
 
-  <h2>Zajímavosti</h2>
-  <hr />
-  
-  <ul>
-    <li><span className='text-highlighter'>Každý student čtvrtého ročníku absolvuje týdenní zkoušku přežití:</span> bez hůlky, bez kouzelného předmětu, s úkolem najít určitou rostlinu nebo zvíře a vrátit se živý. Je to považováno za zkoušku dospělosti. Ještě se nestalo, že by zkouška nebyla splněna.</li>
-    <li>Jednou za dvanáct měsíců se koná <span className='text-highlighter'>rituál „Zhasnutí měsíce“,</span> během něhož studenti pomocí starodávných kouzel "zakryjí" měsíc obřadním kouřem z posvátných bylin. Jde o symbolické přerušení času a cyklů, které studentům umožňuje spirituálně „začít znovu“.</li>
-    <li>K tajemstvím školních pozemků patří také <span className='text-highlighter'>strom Xitapê</span>, který dokáže zprostředkovat zprávy na dálku. Když člověk položí ruku na jeho kmen a myslí na určitou osobu, strom vyryje vzkaz do svého listu. Ten se pak odlomí a dorazí až k adresátovi, ať se nachází kdekoli. Tento způsob komunikace byl obzvlášť ceněný během období izolace školy.</li>
-    <li><span className='text-highlighter'>Velká část všech spořádaných plodin je pěstována na škole</span>. Studenti se podílejí a škola je tak do jisté míry soběstačnou jednotkou. Maso a jiné suroviny pak buď nakupují, anebo mění s okolními kmeny, které peníze neznají a neužívají.</li>
-  </ul>
    
 
 
