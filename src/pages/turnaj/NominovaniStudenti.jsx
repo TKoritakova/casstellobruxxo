78becff3004334d8760e5c905ad78aa405e6a873
Štěpán Šprincl <sprinste@cvut.cz>
2026-03-03 16:29:17 +0100

    Student profiles linked

--- src/pages/turnaj/NominovaniStudenti.jsx
+++ src/pages/turnaj/NominovaniStudenti.jsx
@@ -28,6 +28,12 @@ import Carmila from '../../img/postavy/carmila_iris_jose.png';
 import Demetrio from '../../img/postavy/demetrio_agamez.png';
 import Jared from '../../img/postavy/jared_ilai_moreno.png';
 import Tristan from '../../img/postavy/tristan_yeray_moreno.png';
+import Archeologie from "../../img/turnaj/seminare/archeologie.png";
+import Artefakty from "../../img/turnaj/seminare/artefakty.png";
+import Komunikace from "../../img/turnaj/seminare/komunikace.png";
+import Kanoe from "../../img/turnaj/seminare/kanoe.png";
+import Ritualka from "../../img/turnaj/seminare/ritualni_magie.png";
+import Zveromagie from "../../img/turnaj/seminare/zveromagie.png";
 
 const NominovaniStudenti = () => (
   <div>
@@ -39,113 +45,131 @@ const NominovaniStudenti = () => (
     <h2>Pátý ročník</h2>
     <hr/>
     <div className='face-cards-wrapper'>
-
-
-      <div className='student-face-card'>
-        <img src={AlejandroS} className='img-character'/>
-        <p>Alejandro Sánchez</p>
+      <div className='seminar-face-card'>
+        <Link to="/alejandro-sanchez" target='_blank'>
+          <img src={AlejandroS} className='img-character'/>
+          <p><span className='text-highlighter'>Alejandro Sánchez</span></p>
+        </Link>
       </div>
-      <div className='student-face-card'>
-        <img src={Catriel} className='img-character'/>
-        <p>Catriel Ulises Condori</p>
+      <div className='seminar-face-card'>
+        <Link to="/catriel-ulises-condori" target='_blank'>
+          <img src={Catriel} className='img-character'/>
+          <p><span className='text-highlighter'>Catriel Ulises Condori</span></p>
+        </Link>
       </div>
-      <div className='student-face-card'>
-        <img src={Kira} className='img-character'/>
-        <p>Kira Soraia Rodrigues</p>
+      <div className='seminar-face-card'>
+        <Link to="/kira-soraia-rodrigues" target='_blank'>
+          <img src={Kira} className='img-character'/>
+          <p><span className='text-highlighter'>Kira Soraia Rodrigues</span></p>
+        </Link>
       </div>
-
-      <div className='student-face-card'>
-        <img src={Lucia} className='img-character'/>
-        <p>Lucia Juliana Navarro</p>
+      <div className='seminar-face-card'>
+        <Link to="/lucia-juliana-navarro" target='_blank'>
+          <img src={Lucia} className='img-character'/>
+          <p><span className='text-highlighter'>Lucia Juliana Navarro</span></p>
+        </Link>
       </div>
-      <div className='student-face-card'>
-        <img src={Melissa} className='img-character'/>
-        <p>Melissa Nayra de León</p>
+      <div className='seminar-face-card'>
+        <Link to="/melissa-nayra-de-leon" target='_blank'>
+          <img src={Melissa} className='img-character'/>
+          <p><span className='text-highlighter'>Melissa Nayra de León</span></p>
+        </Link>
       </div>
-
-
-      <div className='student-face-card'>
-        <img src={SofiaS} className='img-character'/>
-        <p>Sofía Sánchez</p>
+      <div className='seminar-face-card'>
+        <Link to="/sofia-sanchez" target='_blank'>
+          <img src={SofiaS} className='img-character'/>
+          <p><span className='text-highlighter'>Sofía Sánchez</span></p>
+        </Link>
       </div>
-
     </div>
-
 
     <h2>Šestý ročník</h2>
     <hr/>
     <div className='face-cards-wrapper'>
-      <div className='student-face-card'>
-        <img src={Cesar} className='img-character'/>
-        <p>César Jesus Giménez Castillero</p>
+      <div className='seminar-face-card'>
+        <Link to="/cesar-jesus-gimenez-castillero" target='_blank'>
+          <img src={Cesar} className='img-character'/>
+          <p><span className='text-highlighter'>César Jesus Giménez Castillero</span></p>
+        </Link>
       </div>
-
-      <div className='student-face-card'>
-        <img src={Ines} className='img-character'/>
-        <p>Inés Soraya Caro</p>
+      <div className='seminar-face-card'>
+        <Link to="/ines-soraya-caro" target='_blank'>
+          <img src={Ines} className='img-character'/>
+          <p><span className='text-highlighter'>Inés Soraya Caro</span></p>
+        </Link>
       </div>
-
-      <div className='student-face-card'>
-        <img src={Juan} className='img-character'/>
-        <p>Juan Miguel Santos Ramírez</p>
+      <div className='seminar-face-card'>
+        <Link to="/juan-miguel-santos-ramirez" target='_blank'>
+          <img src={Juan} className='img-character'/>
+          <p><span className='text-highlighter'>Juan Miguel Santos Ramírez</span></p>
+        </Link>
       </div>
-
-      <div className='student-face-card'>
-        <img src={Luan} className='img-character'/>
-        <p>Luan Caetanno</p>
+      <div className='seminar-face-card'>
+        <Link to="/luan-caetanno" target='_blank'>
+          <img src={Luan} className='img-character'/>
+          <p><span className='text-highlighter'>Luan Caetanno</span></p>
+        </Link>
       </div>
-
-      <div className='student-face-card'>
-        <img src={Mateo} className='img-character'/>
-        <p>Mateo Benítez</p>
+      <div className='seminar-face-card'>
+        <Link to="/mateo-benitez" target='_blank'>
+          <img src={Mateo} className='img-character'/>
+          <p><span className='text-highlighter'>Mateo Benítez</span></p>
+        </Link>
       </div>
-
-      <div className='student-face-card'>
-        <img src={Mikkel} className='img-character'/>
-        <p>Mikel Ancco de León</p>
+      <div className='seminar-face-card'>
+        <Link to="/mikel-ancco-de-leon" target='_blank'>
+          <img src={Mikkel} className='img-character'/>
+          <p><span className='text-highlighter'>Mikel Ancco de León</span></p>
+        </Link>
       </div>
-      <div className='student-face-card'>
-        <img src={SofiaP} className='img-character'/>
-        <p>Sofia Amarilis Perez</p>
+      <div className='seminar-face-card'>
+        <Link to="/sofia-amarilis-perez" target='_blank'>
+          <img src={SofiaP} className='img-character'/>
+          <p><span className='text-highlighter'>Sofia Amarilis Perez</span></p>
+        </Link>
       </div>
-      <div className='student-face-card'>
-        <img src={YaraS} className='img-character'/>
-        <p>Yara Quetzal do Sol</p>
+      <div className='seminar-face-card'>
+        <Link to="/yara-quetzal-do-sol" target='_blank'>
+          <img src={YaraS} className='img-character'/>
+          <p><span className='text-highlighter'>Yara Quetzal do Sol</span></p>
+        </Link>
       </div>
-
-      <div className='student-face-card'>
-        <img src={YaraM} className='img-character'/>
-        <p>Yara Monteiro</p>
+      <div className='seminar-face-card'>
+        <Link to="/yara-monteiro" target='_blank'>
+          <img src={YaraM} className='img-character'/>
+          <p><span className='text-highlighter'>Yara Monteiro</span></p>
+        </Link>
       </div>
-
     </div>
-
 
     <h2>Sedmý ročník</h2>
     <hr/>
-
     <div className='face-cards-wrapper'>
-      <div className='student-face-card'>
-        <img src={AlejandroN} className='img-character'/>
-        <p>Alejandro Navarro</p>
+      <div className='seminar-face-card'>
+        <Link to="/alejandro-navarro" target='_blank'>
+          <img src={AlejandroN} className='img-character'/>
+          <p><span className='text-highlighter'>Alejandro Navarro</span></p>
+        </Link>
       </div>
-
-      <div className='student-face-card'>
-        <img src={Chiara} className='img-character'/>
-        <p>Chiara Antonia José</p>
+      <div className='seminar-face-card'>
+        <Link to="/chiara-antonia-jose" target='_blank'>
+          <img src={Chiara} className='img-character'/>
+          <p><span className='text-highlighter'>Chiara Antonia José</span></p>
+        </Link>
       </div>
-
-      <div className='student-face-card'>
-        <img src={Gabriel} className='img-character'/>
-        <p>Gabriel Sebastian Almeida Soares</p>
+      <div className='seminar-face-card'>
+        <Link to="/gabriel-sebastian-almeida-soares" target='_blank'>
+          <img src={Gabriel} className='img-character'/>
+          <p><span className='text-highlighter'>Gabriel Sebastian Almeida Soares</span></p>
+        </Link>
       </div>
-      <div className='student-face-card'>
-        <img src={Luana} className='img-character'/>
-        <p>Luana Mariana Almeida Soares</p>
+      <div className='seminar-face-card'>
+        <Link to="/luana-mariana-almeida-soares" target='_blank'>
+          <img src={Luana} className='img-character'/>
+          <p><span className='text-highlighter'>Luana Mariana Almeida Soares</span></p>
+        </Link>
       </div>
-
     </div>
-
 
     <h2>Profesorský dozor</h2>
     <hr/>
@@ -153,27 +177,27 @@ const NominovaniStudenti = () => (
     <div className='face-cards-wrapper'>
       <div className='student-face-card'>
         <img src={Murhala} className='img-character'/>
-        <p>Caetano Murhala</p>
+        <p><span className='text-highlighter'>Caetano Murhala</span></p>
       </div>
       <div className='student-face-card'>
         <img src={Carmila} className='img-character'/>
-        <p>Carmila Iris José</p>
+        <p><span className='text-highlighter'>Carmila Iris José</span></p>
       </div>
       <div className='student-face-card'>
         <img src={Demetrio} className='img-character'/>
-        <p>Demetrio Agamez</p>
+        <p><span className='text-highlighter'>Demetrio Agamez</span></p>
       </div>
       <div className='student-face-card'>
         <img src={Ernesto} className='img-character'/>
-        <p>Ernesto Azevedo</p>
+        <p><span className='text-highlighter'>Ernesto Azevedo</span></p>
       </div>
       <div className='student-face-card'>
         <img src={Jared} className='img-character'/>
-        <p>Jared Ilai Moreno</p>
+        <p><span className='text-highlighter'>Jared Ilai Moreno</span></p>
       </div>
       <div className='student-face-card'>
         <img src={Tristan} className='img-character'/>
-        <p>Tristán Yeray Moreno</p>
+        <p><span className='text-highlighter'>Tristán Yeray Moreno</span></p>
       </div>
 
 
