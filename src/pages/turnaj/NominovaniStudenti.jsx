911c1fc4fa5a315dfa4dd0c00f85e14be7f7c14d
Štěpán Šprincl <sprinste@cvut.cz>
2026-03-15 10:09:55 +0100

    Fixed Jared's image

--- src/pages/turnaj/NominovaniStudenti.jsx
+++ src/pages/turnaj/NominovaniStudenti.jsx
@@ -195,7 +195,7 @@ const NominovaniStudenti = () => (
         <img src={Ernesto} className='img-character'/>
         <p><span className='text-highlighter'>Ernesto Azevedo</span></p>
       </div>
-      <div className='seminar-face-card'>
+      <div className='clickable-face-card'>
         <Link to="/jared-ilai-moreno" target='_blank'>
         <img src={Jared} className='img-character'/>
         <p><span className='text-highlighter'>Jared Ilai Moreno</span></p>
